1a0995659a8499661f8fa6985f729a05b12708d5
Jorge Contreras <jorge.contreras789@gmail.com>
2020-06-02 13:04:21 -0400

    listen to user input and update the user score based on the input

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import {
   StyledGame,
   StyledScore,
@@ -9,12 +9,15 @@ import { Strong } from '../styled/Reusable';
 
 export default function Game({ history }) {
   const MAX_SECONDS = 5;
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  const [currentCharacter, setCurrentCharacter] = useState('');
   const [score, setScore] = useState(0);
   const [ms, setMs] = useState(0);
   const [seconds, setSeconds] = useState(MAX_SECONDS);
 
   useEffect(() => {
+    setRandomCharacter();
     const currentTime = new Date();
     const interval = setInterval(() => updateTime(currentTime), 1);
     return () => clearInterval(interval);
@@ -41,12 +44,38 @@ export default function Game({ history }) {
     }
   }, [seconds, ms, history]);
 
+  const keyUpHandler = useCallback(
+    (e) => {
+      if (e.key === currentCharacter) {
+        setScore((prevScore) => prevScore + 1);
+      } else {
+        if (score > 0) {
+          setScore((prevScore) => prevScore - 1);
+        }
+      }
+      setRandomCharacter();
+    },
+    [score, currentCharacter]
+  );
+
+  useEffect(() => {
+    document.addEventListener('keyup', keyUpHandler);
+    return () => {
+      document.removeEventListener('keyup', keyUpHandler);
+    };
+  }, [keyUpHandler]);
+
+  const setRandomCharacter = () => {
+    const randomInt = Math.floor(Math.random() * 36);
+    setCurrentCharacter(characters[randomInt]);
+  };
+
   return (
     <StyledGame>
       <StyledScore>
         Score:<Strong>{score}</Strong>
       </StyledScore>
-      <StyledCharacter>A</StyledCharacter>
+      <StyledCharacter>{currentCharacter}</StyledCharacter>
       <StyledTimer>
         Time:{' '}
         <Strong>
