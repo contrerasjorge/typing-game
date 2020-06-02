6e733b1df5fbc670fbb179d9a4234d597bd9a8bf
Jorge Contreras <jorge.contreras789@gmail.com>
2020-06-02 07:29:01 -0400

    initial setup, added routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Game from "./pages/Game";
+import HighScores from "./pages/HighScores";
+import GameOver from "./pages/GameOver";
+import Home from "./pages/Home";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/game" component={Game} />
+        <Route path="/highScores" component={HighScores} />
+        <Route path="/gameOver" component={GameOver} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
