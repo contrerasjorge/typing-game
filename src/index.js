7105ec92ed6a4a3949be7ec2ddca1bb247fd7bc4
Jorge Contreras <jorge.contreras789@gmail.com>
2020-06-04 09:44:12 -0400

    including access token in the save high schore call

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ ReactDOM.render(
     client_id={config.clientId}
     redirect_uri={window.location.origin}
     onRedirectCallback={onRedirectCallback}
+    audience={config.audience}
   >
     <React.StrictMode>
       <ScoreProvider>
--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -3,10 +3,12 @@ import { StyledLink } from '../styled/Navbar';
 import { StyledCharacter } from '../styled/Game';
 import { useScore } from '../contexts/ScoreContext';
 import { StyledTitle } from '../styled/Reusable';
+import { useAuth0 } from '../auth';
 
 export default function GameOver({ history }) {
   const [score] = useScore();
   const [scoreMessage, setScoreMessage] = useState('');
+  const { getTokenSilently, isAuthenticated } = useAuth0();
 
   if (score === -1) {
     history.push('/');
@@ -17,9 +19,13 @@ export default function GameOver({ history }) {
 
     const saveHighScore = async () => {
       try {
+        const token = await getTokenSilently();
         const options = {
           method: 'POST',
-          body: JSON.stringify({ name: 'Jimmy', score }),
+          body: JSON.stringify({ name: 'Paul', score }),
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         };
         const res = await fetch('/.netlify/functions/saveHighScore', options);
         const data = await res.json();
@@ -36,13 +42,18 @@ export default function GameOver({ history }) {
         console.error(err);
       }
     };
-    saveHighScore();
-  }, [score]);
+    if (isAuthenticated) {
+      saveHighScore();
+    }
+  }, [score, isAuthenticated, getTokenSilently]);
 
   return (
     <div>
       <StyledTitle>GameOver</StyledTitle>
       <h2>{scoreMessage}</h2>
+      {!isAuthenticated && (
+        <h2>You should log in or signup to compete for high scores!</h2>
+      )}
       <StyledCharacter>{score}</StyledCharacter>
       <div>
         <StyledLink to="/">Go Home</StyledLink>
