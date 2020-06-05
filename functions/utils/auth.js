d6a9b6a92090e4878a7cc475ada9911b2442970b
Jorge Contreras <jorge.contreras789@gmail.com>
2020-06-05 01:36:19 -0400

    fixed a typo

--- functions/utils/auth.js
+++ functions/utils/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const jwks = require('jwks-rsa');
 const jwksClient = jwks({
-  jwtsUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
   audience: process.env.AUTH0_AUDIENCE,
 });
 const { promisify } = require('util');
@@ -11,12 +11,10 @@ const getAccessTokenFromHeaders = (headers) => {
   if (!rawAuthorization) {
     return null;
   }
-
   const authorizationParts = rawAuthorization.split(' ');
   if (authorizationParts[0] !== 'Bearer' || authorizationParts.length !== 2) {
     return null;
   }
-
   const accessToken = authorizationParts[1];
   return accessToken;
 };
@@ -32,7 +30,6 @@ const validateAccessToken = async (token) => {
 
     const options = { algorithms: alg };
     jwt.verify(token, signingKey, options);
-
     return decodedToken.payload;
   } catch (err) {
     console.error(err);
